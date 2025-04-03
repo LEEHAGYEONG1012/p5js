f699ca5cb5212e408ed94f0022a72e49189b0b4b
LEEHAGYEONG1012 <hk041012@naver.com>
2025-04-03 09:40:42 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let circleX=200
+let circleY=200
+let speed=5
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  if (keyIsPressed){
+    if(keyCode===LEFT_ARROW){
+      circleX -= speed}
+    else if (keyCode===RIGHT_ARROW){
+      circleX+=speed}
+    else if (keyCode===UP_ARROW){
+      circleY-=speed}
+    else if (keyCode===DOWN_ARROW){
+      circleY+=speed}
+  }
+  
+  ellipse(circleX,circleY,50,50)
+  Fill('#CC0000')
+}
